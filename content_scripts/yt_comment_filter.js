1370194c8707ee2c2a8d993e98ccc680789f523e
Cawdabra <cawdabra@null.net>
2017-11-12 00:51:48 -0400

    Implemented comment filtering and word censoring.

--- content_scripts/yt_comment_filter.js
+++ content_scripts/yt_comment_filter.js
@@ -12,20 +12,20 @@ var NODE_PROPAGATION_WAIT_PERIOD = 400;
 var DesktopSelectors = {
     COMMENT: "section.comment-thread-renderer",
     COMMENT_TEXT: "div.comment-renderer > div.comment-renderer-content > div.comment-renderer-text > div.comment-renderer-text-content",
-    REPLY: "section.comment-thread-renderer > div.comment-replies-renderer",
-    REPLY_TEXT: "div.comment-renderer > div.comment-renderer-content > div.comment-renderer-text > div.comment-renderer-text-content",
+    REPLY: "div.comment-renderer",
+    REPLY_TEXT: "div.comment-renderer-content > div.comment-renderer-text > div.comment-renderer-text-content",
     UNLOADED_DISCUSSION: "div#watch-discussion",
     LOADED_DISCUSSION: "div#comment-section-renderer-items",
     UNEXPANDED_REPLIES: "div.comment-replies-renderer .yt-uix-expander-body.comment-replies-renderer-pages"
 };
 
 var MobileSelectors = {
-    COMMENT: "div.koya-komponent-binding:first-child",
+    COMMENT: "div._mkdc",
     COMMENT_TEXT: "div._mldc div._mdec > span._mzgc",
     REPLY: "div._mwdc > div.koya-komponent-binding",
     REPLY_TEXT: "div._mdec > span._mzgc",
-    UNLOADED_DISCUSSION: "div._midc._mgnb._mfnb",
-    LOADED_DISCUSSION: "div._midc._mgnb._mfnb",     // Parent of this
+    UNLOADED_DISCUSSION: "div._midc._mgnb._mfnb",       // Parent of this
+    LOADED_DISCUSSION: "",                              // Not used since mobile uses the same element for unloaded and loaded
     UNEXPANDED_REPLIES: "div._mkdc > div.koya-komponent-binding"
 };
 
@@ -36,13 +36,17 @@ var gDiscussionObserver = void 0;
 var gDiscussionElement = void 0;
 var gDiscussionsLoaded = false;
 
-var gCommentFilterEnabled = void 0;
-var gWordCensorEnabled = void 0;
+var gReplyObservers = [];
 
 var gCommentFilterRegExps = [];
-var gCensoredWords = [];
+var gWordCensorRegExps = [];
 
 var gCurrentCommentIndex = 0;
+
+// Settings
+var gCommentFilterEnabled = void 0;
+var gWordCensorEnabled = void 0;
+var gHideFilteredComments = void 0;
 
 function init() {
     var desktop_video_regex = /(http|https):\/\/(www\.)?youtube.com\/watch\?.*/i;
@@ -76,7 +80,7 @@ function init() {
 }
 
 function setupCommentFilter() {
-    chrome.storage.local.get(["commentFilterEnabled", "commentFilter"], function(items) {
+    chrome.storage.local.get(["commentFilterEnabled", "commentFilter", "hideFilteredComments"], function(items) {
         var error = chrome.runtime.lastError;
         if (error) {
             console.log("Error:", error);
@@ -112,6 +116,8 @@ function setupCommentFilter() {
                     }
                 }
             }
+
+            gHideFilteredComments = items.hideFilteredComments;
         }
     });
 }
@@ -125,17 +131,29 @@ function setupWordCensor() {
         else {
             gWordCensorEnabled = items.wordCensorEnabled;
             if (gWordCensorEnabled) {
-                gCensoredWords = [];
-                var words = item.wordCensor.split("\n");
+                gWordCensorRegExps = [];
+                var words = items.wordCensor.split("\n");
+                var regex = void 0;
                 for (var i=0; i<words.length; ++i) {
-                    gCensoredWords.push(words[i].toLowerCase());
+                    regex = new RegExp("\\b" + words[i] + "\\b", "ig");
+                    regex.word = words[i];
+                    gWordCensorRegExps.push(regex);
                 }
             }
         }
     });
 }
 
-function onDiscussionObserverTimeout(e) {
+function discussionObserverCallback(mutation) {
+    for (var i=0; i<mutation.length; ++i) {
+        if (mutation[i].addedNodes.length > 0) {
+            setTimeout(onDiscussionObserverTimeout, NODE_PROPAGATION_WAIT_PERIOD);
+            break;
+        }
+    }
+}
+
+function onDiscussionObserverTimeout() {
     if (gDeviceType === "desktop" && !gDiscussionsLoaded) {
         var discussion_element = document.querySelector(gSelectors.LOADED_DISCUSSION);
         if (discussion_element) {
@@ -146,36 +164,99 @@ function onDiscussionObserverTimeout(e) {
 
             gDiscussionObserver = new MutationObserver(discussionObserverCallback);
             gDiscussionObserver.observe(discussion_element, { childList: true });
-            processComments();
+            var comment_elements = Array.prototype.slice.call(gDiscussionElement.querySelectorAll(gSelectors.COMMENT), gCurrentCommentIndex);
+            processComments(comment_elements, gSelectors.COMMENT_TEXT);
+            gCurrentCommentIndex += comment_elements.length;
         }
     }
     else {
-        processComments();
+        var comment_elements = Array.prototype.slice.call(gDiscussionElement.querySelectorAll(gSelectors.COMMENT), gCurrentCommentIndex);
+        processComments(comment_elements, gSelectors.COMMENT_TEXT);
+        if (gDeviceType === "desktop") {
+            gCurrentCommentIndex += comment_elements.length;
+        }
     }
 }
 
-function discussionObserverCallback(mutation) {
+function unexpandedRepliesObserverCallback(mutation) {
     var nodes_added = false;
+    var target = void 0;
     for (var i=0; i<mutation.length; ++i) {
         if (mutation[i].addedNodes.length > 0) {
-            nodes_added = true;
+            // Kill the observer
+            target = mutation[i].target;
+            target.ytfObserver.disconnect();
+            delete target.ytfObserver;
+
+            setTimeout(function() {
+                processComments(Array.prototype.slice.call(target.querySelectorAll(gSelectors.REPLY)), gSelectors.REPLY_TEXT);
+            }, NODE_PROPAGATION_WAIT_PERIOD);
             break;
         }
     }
-
-    if (nodes_added) {
-        setTimeout(onDiscussionObserverTimeout, NODE_PROPAGATION_WAIT_PERIOD);
-    }
 }
 
-function processComments() {
-    var comment_elements = Array.prototype.slice.call(gDiscussionElement.querySelectorAll(gSelectors.COMMENT), gCurrentCommentIndex);
-    var element = void 0;
+function processComments(elements, text_selector) {
+    var text_element = void 0;
     var comment = void 0;
-    for(var i=0; i<comment_elements.length; ++i) {
-        element = comment_elements[i].querySelector(gSelectors.COMMENT_TEXT);
-        if (element) {
-            comment = element.innerHTML;
+    var comment_removed = void 0;
+    var unexpanded_replies_element = void 0;
+    var observer = void 0;
+    var span = void 0;
+    for(var i=0, j=void 0; i<elements.length; ++i) {
+        text_element = elements[i].querySelector(text_selector);
+        if (text_element) {
+            comment_removed = false;
+            comment = text_element.innerHTML;
+            if (gCommentFilterEnabled) {
+                for (j=0; j<gCommentFilterRegExps.length; ++j) {
+                    if (gCommentFilterRegExps[j].test(comment)) {
+                        if (gHideFilteredComments) {
+                            elements[i].parentNode.removeChild(elements[i]);
+                            text_element = null;
+                        }
+                        else {
+                            text_element.style.display = "none";
+                            span = document.createElement("span");
+                            span.style.color = "red";
+                            span.appendChild(document.createTextNode("Comment removed by filter."));
+                            text_element.parentElement.appendChild(span);
+                        }
+
+                        comment_removed = true;
+                        break;
+                    }
+                }
+
+                if (gDeviceType === "mobile" && text_element) {
+                    // Stupid mobile recreates the entire discussion stuff when loading more comments
+                    processComments(Array.prototype.slice.call(elements[i].querySelectorAll(gSelectors.REPLY)), gSelectors.REPLY_TEXT);
+                }
+            }
+
+            if (text_element) {
+                if (gWordCensorEnabled && !comment_removed) {
+                    for (j=0; j<gWordCensorRegExps.length; ++j) {
+                        comment = comment.replace(gWordCensorRegExps[j], "*".repeat(gWordCensorRegExps[j].word.length));
+                    }
+
+                    if (text_element.innerHTML !== comment) {
+                        span = document.createElement("span");
+                        span.className = text_element.className;
+                        span.innerHTML = comment;
+                        text_element.style.display = "none";
+                        text_element.className = "";
+                        text_element.parentElement.appendChild(span);
+                    }
+                }
+
+                unexpanded_replies_element = elements[i].querySelector(gSelectors.UNEXPANDED_REPLIES);
+                if (unexpanded_replies_element) {
+                    observer = new MutationObserver(unexpandedRepliesObserverCallback);
+                    unexpanded_replies_element.ytfObserver = observer;
+                    observer.observe(unexpanded_replies_element, { childList: true });
+                }
+            }
         }
     }
 }
